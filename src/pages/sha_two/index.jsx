7479ba1a42c363985135cc7443082a0cbf66890d
Your Name <vitaliyburdin@MacBook-Pro-Vitaliy.local>
2021-08-11 00:33:31 +0500

    разбираюсь с добавлением единицы

--- src/pages/sha_two/index.jsx
+++ src/pages/sha_two/index.jsx
@@ -2,16 +2,21 @@ import {toUint8Array, addLeadingZero} from './../../helpers'
 import OutputCode from '../../components/OutputCode';
 
 export default function() {
-
-    const uint8Array = toUint8Array('Привет, мир');
+    const msg = 'hi';
+    const uint8Array = toUint8Array(msg);
+    
     console.log(uint8Array);
 
     return (<div>
         <p>Преобразуем входящее сообщение в массив 8-битных целых чисел без знака.</p>
         <OutputCode>
-           {Array.from(uint8Array).map(int => <span>{ addLeadingZero(int.toString(2)) }</span>)}
+           {Array.from(toUint8Array(msg)).map(int => <span>{ addLeadingZero(int.toString(2)) }</span>)}
         </OutputCode>
-       
+        <p>Заменим первый бит первого октета на 1 </p>
+        <OutputCode>
+            {msg + String.fromCharCode(0x80)}
+           {Array.from(toUint8Array(msg + String.fromCharCode(0x80))).map(int => <span>{ addLeadingZero(int.toString(2)) }</span>)}
+        </OutputCode>
         
         <p>преобразуем массив</p>
     </div>)
