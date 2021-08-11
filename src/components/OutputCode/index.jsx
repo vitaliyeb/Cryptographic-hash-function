2bda70c46f60cdfc3e01a14f674c361e154c2467
vitaliyeb <vlvlv.vovov@bk.ru>
2021-08-11 13:01:23 +0500

    перенёс логику на генератор

--- src/components/OutputCode/index.jsx
+++ src/components/OutputCode/index.jsx
@@ -2,7 +2,6 @@ import style from './style.module.css'
 
 
 export default function({ children }) {
-    console.log(style);
     return (
         <pre className={style.codeView}>{ children }</pre>
         )
--- src/pages/sha_two/index.jsx
+++ src/pages/sha_two/index.jsx
@@ -2,20 +2,25 @@ import {toUint8Array, addLeadingZero} from './../../helpers'
 import OutputCode from '../../components/OutputCode';
 
 export default function() {
-    const msg = 'hitest';
-    const uint8Array = toUint8Array(msg);
-    
-    console.log(uint8Array);
+    const msg = 'hi';
+    const generate = (function* (msg) {
+        let firstBuffer = toUint8Array(msg);
+        yield firstBuffer;
+        let buffer = new Uint8Array([...firstBuffer, 128]);
+        yield buffer;
+    })(msg);
+
+    console.log();
 
     return (<div>
         <p>Преобразуем входящее сообщение в массив 8-битных целых чисел без знака.</p>
         <OutputCode>
-           {Array.from(toUint8Array(msg)).map(int => <span>{ addLeadingZero(int.toString(2)) }</span>)}
+            {Array.from(generate.next().value).map((i, k) => <span key={k}>{ addLeadingZero(i.toString(2)) }</span>)}
         </OutputCode>
         <p>Заменим первый бит первого октета на 1 </p>
         <OutputCode>
-            {msg + String.fromCharCode(0x80)}
-           {Array.from(toUint8Array(msg + String.fromCharCode(0x80))).map(int => <span>{ addLeadingZero(int.toString(2)) }</span>)}
+            {/* { console.log(generate.next().value) } */}
+            {Array.from(generate.next().value).map(int => <span>{ addLeadingZero(int.toString(2)) }</span>)}
         </OutputCode>
         
         <p>преобразуем массив</p>
